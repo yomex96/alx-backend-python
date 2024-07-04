9252c7f6821ddbca41df32180f05caf3cd2bf4ff
yomex96 <abayomirobertonawole2@gmail.com>
2024-07-04 02:30:43 -0700

    past

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list.py"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum_mixed_list function"""
+    return sum(mxd_lst)
